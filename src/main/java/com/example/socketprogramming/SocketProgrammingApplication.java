ed0d103cadaee08d3bfab62aad5e58ca4045d71a
Bethwel Karanja <101641595+bethwelamkenya@users.noreply.github.com>
2023-04-27 13:13:09 +0300

    Socket Programming

---
+++ src/main/java/com/example/socketprogramming/SocketProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socketprogramming/SocketProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
